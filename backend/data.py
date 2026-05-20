5133d0c6195a03d738376caaa4269f14f709e04f
Tamarapalliakash133 <akashtamarapalli43@gmail.com>
2026-05-20 22:55:26 +0530

    last-commit

--- backend/data.py
+++ backend/data.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi-pratice.db"
+import os
+DB_PATH = os.path.join(os.path.dirname(__file__), "fastapi-pratice.db")
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,connect_args = {"check_same_thread" : False}
