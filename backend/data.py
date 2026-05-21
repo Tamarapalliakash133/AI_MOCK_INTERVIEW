d47c60712efc05631013bb58a1c04d7c17ca80cb
Tamarapalliakash133 <akashtamarapalli43@gmail.com>
2026-05-21 12:12:24 +0530

    final-commit

--- backend/data.py
+++ backend/data.py
@@ -3,14 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import os
-DB_PATH = os.environ.get("DB_PATH", "/tmp/fastapi-pratice.db")
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,connect_args = {"check_same_thread" : False}
-)
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "")
 
-SessionLocal = sessionmaker(autocommit = False,autoflush = False,bind = engine)
+
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
